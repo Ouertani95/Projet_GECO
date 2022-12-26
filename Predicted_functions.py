313dd0b71f55054dd616f5bbb9de350488f9d976
Mohamed Ouertani <mohamed.ouertani.1@etu.univ-amu.fr>
2022-12-26 09:16:00 +0100

    modified Predicted_functions script to work for all species

--- Predicted_functions.py
+++ Predicted_functions.py
@@ -1,19 +1,25 @@
+import os
+
 import pandas
 import pandas as pd
 
-A_hemo = 'Arcanobacterium_hemolyticum_DSM_20595_GCF_000092365.1_MM_k1jt2xw8.emapper.annotations.tsv'
+if  not os.path.exists('Predicted_functions'):
+    os.mkdir('Predicted_functions')
 
-# Read file
-table = pd.read_csv(f'Eggnog_results/{A_hemo}', delimiter="\t")
-is_general = (table["COG_category"] == "-") | (table["COG_category"] == "S") | (table["COG_category"] == "R")
-table = table[~ is_general]
-# Count categories
-values_count = table["Description"].value_counts().to_dict()
-predicted_functions = pd.DataFrame(columns=["Predicted function", "Count"])
-for function, count in values_count.items():
-    new_row = pandas.DataFrame([[function, count]], columns=["Predicted function", "Count"])
-    predicted_functions = pd.concat([predicted_functions, new_row])
-# Extract species
-split_file_name = A_hemo.split('_')
-title = f"{split_file_name[0]}_{split_file_name[1]}"
-predicted_functions.to_csv(f"Predicted_functions_{title}.tsv", sep="\t", index=False)
+eggnog_results = os.listdir('Eggnog_results/')
+
+for result in eggnog_results:
+    # Read file
+    table = pd.read_csv(f'Eggnog_results/{result}', delimiter="\t")
+    is_general = (table["COG_category"] == "-") | (table["COG_category"] == "S") | (table["COG_category"] == "R")
+    table = table[~ is_general]
+    # Count categories
+    values_count = table["Description"].value_counts().to_dict()
+    predicted_functions = pd.DataFrame(columns=["Predicted function", "Count"])
+    for function, count in values_count.items():
+        new_row = pandas.DataFrame([[function, count]], columns=["Predicted function", "Count"])
+        predicted_functions = pd.concat([predicted_functions, new_row])
+    # Extract species
+    split_file_name = result.split('_')
+    title = f"{split_file_name[0]}_{split_file_name[1]}"
+    predicted_functions.to_csv(f"Predicted_functions/Predicted_functions_{title}.tsv", sep="\t", index=False)
